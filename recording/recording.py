fda61422624d1545e6e9bd30b693c45e7d2720f1
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2023-05-15 20:52:16 -0400

    Add files via upload

--- recording/recording.py
+++ recording/recording.py
@@ -5,6 +5,7 @@ import pygetwindow as gw
 import time
 import os
 import csv
+import pandas as pd
 
 from pynput import keyboard
 from pynput import mouse
@@ -15,22 +16,74 @@ TEST_FILE = 'D:/Python Projects/gameBot/recording output/gameplay'
 
 class Recorder:
 
+    """
+    Recorder:
+    Description: This class allows us to record the screen, keyboard, and mouse all at the same time.
+                 The screen is output to a video file, while the keyboard/mouse are output to a csv
+
+    Methods:
+    1.) on_press(self, key):
+    2.) on_release(self, key):
+    3.) on_move(self, x, y):
+    4.) on_click(self, x, y, button, pressed):
+    5.) run(self, record_seconds=10):
+    6.) quit(self):
+    7.) future: cleaning methods
+
+    __init__:
+
+    Variables Called:
+    1.) out_filename: The name of the file for the output file. This will be the same for video and csv
+    2.) window_name: What is the name of the window/program we're recording?
+    3.) codec: What codec are we using?
+    4.) fps: At what fps should we record?
+
+    Variables Initialized:
+    1.) self.codec: The codec variable we're using
+    2.) self.window: The window we're recording
+    3.) self.screen_width: The width of the window we're recording
+    4.) self.screen_height: The height of the window we're recording
+    5.) self.screen_size: The tuple of the width by height
+    6.) self.keys_pressed: Keeps track of the keys pressed in a particular frame
+    7.) self.keys_released: Keeps track of the keys released in a particular frame
+    8.) self.mouse_moved: Keeps track of where the mouse moved in a particular frame
+    9.) self.mouse_clicked: Keeps track of where/how the mouse clicked in a particular frame
+    10.) self.keyboard_out: Used to keep track of data for all frames
+    11.) self.keyboard_outfile: The name of the file we're exporting to
+    12.) self.outfile_headers: The headers for the export file
+    """
+
     def __init__(self, out_filename, window_name='minecraft', codec='MJPG', fps=30.):
+        # Initialize the codec we're using
         self.codec = cv2.VideoWriter_fourcc(*'{0}'.format(codec))
+
+        # Grab the window we're recording and its dimensions
         self.window = gw.getWindowsWithTitle(window_name)[0]
         self.screen_width, self.screen_height = self.window.size
         self.screen_size = (self.screen_width, self.screen_height)
 
+        # Initialize video writer to record video
         self.out = cv2.VideoWriter(out_filename + '.avi', self.codec, fps, self.screen_size)
 
+        # Initialize lists used for tracking mouse and keyboard
         self.keys_pressed = []
         self.keys_released = []
         self.mouse_moved = []
         self.mouse_clicked = []
 
+        # Initialize variables needed for creating the outfile
         self.keyboard_out = []
         self.keyboard_outfile = out_filename + '.csv'
         self.outfile_headers = ('timestamp', 'keys_pressed', 'keys_released', 'mouse_moved', 'mouse_clicked')
+
+    """
+    pynput key functions
+    
+    on_press: tells the listener what to do when a key is pressed
+    on_release: tells the listener what to do when a key is released
+    
+    In both instances, we append keys to the appropriate list for their frame
+    """
 
     def on_press(self, key):
         try:
@@ -46,17 +99,26 @@ class Recorder:
             k = key.name
         self.keys_released.append(k)
 
+    """
+    pynput mouse functions
+    
+    on_move: tells the listener what to do when the mouse is moved
+    on_release: tells the listener what to do when the mouse button is released
+    
+    """
+
     def on_move(self, x, y):
         self.mouse_moved.append((x, y))
 
     def on_click(self, x, y, button, pressed):
+        # Determine which button is clicked and save it
         button_clicked = None
-
         if button == mouse.Button.left:
             button_clicked = 'left'
         elif button == mouse.Button.right:
             button_clicked = 'right'
 
+        # Keep track of if the button was pressed or released and save it
         if pressed:
             self.mouse_clicked.append((x, y, button_clicked, 'pressed'))
         else:
@@ -65,8 +127,12 @@ class Recorder:
     def clean_mouse_position(self):
         pass
 
-    def clean_csv(self):
-        pass
+    def clean_output(self):
+        df_out = pd.DataFrame(self.keyboard_out, columns=self.outfile_headers)
+        # Remove empty frames
+        # For each key, track
+        # Keep track of mouse position
+        # Make sure positions are right
 
     def run(self, record_seconds=10):
         start_time = time.time()
