c61d0f7c9c618d4f665a6048962652ff7b5d1ba2
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2023-06-17 17:04:03 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -16,27 +16,9 @@ from keras.layers.core import Activation
 from keras.layers import BatchNormalization
 from keras.layers import Input
 
-TEST_FILE = 'D:/Python Projects/gameBot/recording output/gameplay'
+INPUT_CSV = 'D:/Python Projects/gameBot/processed output/gameplay.csv'
+INPUT_AVI = 'D:/Python Projects/gameBot/recording output/gameplay.avi'
 BATCH_SIZE = 12
-
-
-# Takes a dataframe and returns a copy of that dataframe with empty frames removed
-def remove_empty_frames(df):
-    df_copy = df.copy()
-    i = 0
-    code = '{0} = {0}['.format('df_copy')
-
-    for col in df.columns:
-        if col not in ('timestamp', 'id', 'frame'):
-            code += '({0}[\'{1}\'] != 0.0) '.format('df_copy', col)
-            if i == len(df.columns) - 1:
-                code += ']'
-            else:
-                code += '|'
-        i += 1
-
-    exec(code)
-    return df_copy
 
 
 class VideoParser(tf.keras.utils.Sequence):
@@ -47,13 +29,15 @@ class VideoParser(tf.keras.utils.Sequence):
     """
 
     def __init__(self,                  # self
-                 file_name,             # Name of the file without extension
+                 csv_file_name,             # Name of the file without extension
+                 avi_file_name,
+                 processed_avi_file_name,
                  y_labels,              # Columns we want to predict
                  batch_size=12):        # How many frames are in each batch?
 
         # Use file_name to find the video file and the csv
-        self.video = cv2.VideoCapture(file_name + '.avi')
-        self.csv_file_name = file_name + '.csv'
+        self.video = cv2.VideoCapture(avi_file_name)
+        self.csv_file_name = csv_file_name
 
         # Load headers and save column names we care about
         self.keys_df_headers = pd.read_csv(self.csv_file_name, nrows=1).columns
@@ -61,8 +45,9 @@ class VideoParser(tf.keras.utils.Sequence):
 
         # Save fields related to batches and size of video
         self.fps = self.video.get(cv2.CAP_PROP_FPS)
-        self.num_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
         self.batch_size = batch_size
+        processed_video = cv2.VideoCapture(processed_avi_file_name)
+        self.num_frames = int(processed_video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     """
     __getitem__:
@@ -104,8 +89,13 @@ class VideoParser(tf.keras.utils.Sequence):
         num_batches = int(self.num_frames // self.batch_size)
         return num_batches
 
+    def __parse_video(self, num_frames=0, ids=None, start=0):
+        if num_frames == 0:
+            return self.__parse_video_specific(ids)
+        return self.__parse_video_iterative(num_frames, start)
+
     """
-    __parse_video:
+    __parse_video_iterative:
     Inputs:
         num_frames: The number of frames we want to parse
         start: What frame do we want to start parsing on?
@@ -118,7 +108,7 @@ class VideoParser(tf.keras.utils.Sequence):
     Called By:
         __parse_dataset:
     """
-    def __parse_video(self, num_frames, start=0):
+    def __parse_video_iterative(self, num_frames, start=0):
         self.video.set(cv2.CAP_PROP_POS_FRAMES, start)
 
         frames = []
@@ -134,6 +124,22 @@ class VideoParser(tf.keras.utils.Sequence):
             i += 1
 
         return pd.DataFrame(data={'id': frame_ids, 'frame': frames})
+
+    def __parse_video_specific(self, ids):
+        frames = []
+        frame_ids = []
+
+        for i in ids:
+            self.video.set(cv2.CAP_PROP_POS_FRAMES, int(i))
+            frame_exists, current_frame = self.video.read()
+
+            if frame_exists:
+                frames.append(current_frame)
+                frame_ids.append(i)
+
+        print('frame ids: ', frame_ids)
+        return pd.DataFrame(data={'id': frame_ids, 'frame': frames})
+
 
     """
     __parse_csv:
@@ -160,6 +166,8 @@ class VideoParser(tf.keras.utils.Sequence):
         cols = ['id']
         for label in labels:
             cols.append(label)
+
+        print('csv ids: ', keyboard_df['id'].to_list())
         return keyboard_df[cols]
 
     """
@@ -169,10 +177,9 @@ class VideoParser(tf.keras.utils.Sequence):
         keys_df = self.__parse_csv(num_frames,
                                    start * self.batch_size + 1,
                                    labels)
-        # self.remove_empty_frames(self.keys_df, 'self.keys_df')
+        ids = keys_df['id'].to_list()
         if keys_df.shape[0] != 0:
-            video_df = self.__parse_video(num_frames,
-                                          start * self.batch_size)
+            video_df = self.__parse_video(ids=ids)
             data = keys_df.merge(video_df, left_on='id', right_on='id')
             return data
         else:
@@ -187,11 +194,13 @@ class VideoParser(tf.keras.utils.Sequence):
 
 class KeyModel:
 
-    def __init__(self, input_shape, initial_learn_rate, epochs):
+    def __init__(self, input_shape, initial_learn_rate=.004, epochs=20, batch_size=12, keys=('w_press', 'w_release')):
         # [BATCH_SIZE, 876, 1616, 3]
         self.input_shape = input_shape
         self.initial_learn_rate = initial_learn_rate
         self.epochs = epochs
+        self.batch_size = batch_size
+        self.keys = keys
         self.model = None
 
         self.assemble_model()
@@ -199,18 +208,43 @@ class KeyModel:
         self.fit_model()
 
     def make_default_hidden_layers(self, data):
-        x = Conv2D(16, (3, 3), padding='same', input_shape=self.input_shape)(data)
+        # 2D Convolutional layer: good for pictures
+        # filters: The number of output filters in the convolution
+        # kernel_size: The height and width of the 2D convolution window
+        # padding: 'valid' means no padding
+        # padding: 'same' results in even padding to the left/right
+        # padding: 'same' with strides=1 means output has the same size as input
+        x = Conv2D(filters=16, kernel_size=(3, 3), padding='same', input_shape=self.input_shape)(data)
+
+        # ReLu: 0 if x < 0, otherwise x=y
         x = Activation('relu')(x)
+
+        # Normalizes inputs
+        # Keeps the mean output close to 0 and the output stdev close to 1
+        # During training (when using fit()), normalizes using mean/stdev from current batch
+        # During inference (when using predict()), normalizes output using moving average of mean/stdev
+        # axis: The axis that should be normalized
         x = BatchNormalization(axis=-1)(x)
+
+        # Downsamples the input by taking the maximum value over an input window
+        # pool_size: The window size over which to take the maximum
+        # Strides (default None): Specifies how far the pooling window moves for each pooling step
+        # If strides is None, it will default to pool_size
         x = MaxPooling2D(pool_size=(3, 3))(x)
+
+        # Randomly sets input units to 0
+        # This is only true during training
+        # rate: float between 0 and 1 that specifies the fraction of the input units to drop
         x = Dropout(0.25)(x)
 
+        # Add another group of layers similar to those explained above
         x = Conv2D(32, (3, 3), padding='same')(x)
         x = Activation('relu')(x)
         x = BatchNormalization(axis=-1)(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
         x = Dropout(0.25)(x)
 
+        # Add another group of layers similar to those explained above
         x = Conv2D(32, (3, 3), padding='same')(x)
         x = Activation('relu')(x)
         x = BatchNormalization(axis=-1)(x)
@@ -218,59 +252,77 @@ class KeyModel:
         x = Dropout(0.25)(x)
         return x
 
-    def w_press(self, data):
+    def add_key_branch(self, data, key_name):
         x = self.make_default_hidden_layers(data)
-        x = Flatten()(x)
-        x = Dense(128)(x)
-        x = Activation('relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.5)(x)
-        x = Dense(2)(x)
-        x = Activation('sigmoid', name='w_press')(x)
-        return x
 
-    def w_release(self, data):
-        x = self.make_default_hidden_layers(data)
+        # Flattens the input
         x = Flatten()(x)
-        x = Dense(128)(x)
-        x = Activation('relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.5)(x)
-        x = Dense(2)(x)
-        x = Activation('sigmoid', name='w_release')(x)
-        return x
 
-    def key_press_release(self, name):
-        pass
+        # A layer in which every neuron from the previous layer is connected to every neuron of the next layer
+        # units: Dimensionality of the output space (number of neurons)
+        x = Dense(128)(x)
+
+        # ReLu: 0 if x < 0, otherwise x=y
+        x = Activation('relu')(x)
+
+        # Normalizes inputs
+        # Keeps the mean output close to 0 and the output stdev close to 1
+        # During training (when using fit()), normalizes using mean/stdev from current batch
+        # During inference (when using predict()), normalizes output using moving average of mean/stdev
+        x = BatchNormalization()(x)
+
+        # Randomly sets input units to 0
+        # This is only true during training
+        # rate: float between 0 and 1 that specifies the fraction of the input units to drop
+        x = Dropout(0.5)(x)
+
+        # A layer in which every neuron from the previous layer is connected to every neuron of the next layer
+        # units: Dimensionality of the output space (number of neurons)
+        x = Dense(2)(x)
+
+        # sigmoid(x) = 1 / (1 + exp(-x))
+        # sigmoid is good for classification
+        x = Activation('sigmoid', name=key_name)(x)
+        return x
 
     def assemble_model(self):
         inputs = Input(shape=self.input_shape)
 
-        w_press_branch = self.w_press(inputs)
-        w_release_branch = self.w_release(inputs)
+        branches = []
+        for key in self.keys:
+            branch = self.add_key_branch(inputs, key)
+            branches.append(branch)
 
         self.model = Model(inputs=inputs,
-                           outputs=[w_press_branch, w_release_branch],
+                           outputs=branches,
                            name='tree_farm')
 
     def compile_model(self):
         optimizer = Adam(learning_rate=self.initial_learn_rate,
                          decay=self.initial_learn_rate / self.epochs)
+
+        loss = {}
+        loss_weights = {}
+        metrics = {}
+
+        for key in self.keys:
+            loss[key] = 'binary_crossentropy'
+            loss_weights[key] = 0.1
+            metrics[key] = 'accuracy'
+
         self.model.compile(optimizer=optimizer,
-                           loss={'w_press': 'binary_crossentropy',
-                                 'w_release': 'binary_crossentropy'},
-                           loss_weights={'w_press': 0.1,
-                                         'w_release': 0.1},
-                           metrics={'w_press': 'accuracy',
-                                    'w_release': 'accuracy'})
+                           loss=loss,
+                           loss_weights=loss_weights,
+                           metrics=metrics)
         
     # https://pyimagesearch.com/2018/12/24/how-to-use-keras-fit-and-fit_generator-a-hands-on-tutorial/
     # Current main problem: expects 1 input but is looking at all 12
     def fit_model(self):
-        batch_size = 12
-
-        train_generator = VideoParser(TEST_FILE, ['w_press', 'w_release'], batch_size=batch_size)
-        test_generator = VideoParser(TEST_FILE, ['w_press', 'w_release'], batch_size=batch_size)
+        train_generator = VideoParser(INPUT_CSV, INPUT_AVI,
+                                      'D:/Python Projects/gameBot/processed output/gameplay.avi',
+                                      self.keys, batch_size=self.batch_size)
+        test_generator = VideoParser(INPUT_CSV, INPUT_AVI, 'D:/Python Projects/gameBot/processed output/gameplay.avi',
+                                     self.keys, batch_size=self.batch_size)
 
         train = self.model.fit(train_generator,
                                validation_data=test_generator,
@@ -283,16 +335,4 @@ class KeyModel:
 # https://stackoverflow.com/questions/47665391/keras-valueerror-input-0-is-incompatible-with-layer-conv2d-1-expected-ndim-4
 # https://stackoverflow.com/questions/67345171/valueerror-input-0-of-layer-conv2d-is-incompatible-with-the-layer-expected-m?rq=3
 def main():
-    batch_size = 12
-    shape = [876, 1616, 3]
-
-    parser = VideoParser(TEST_FILE)
-    df = parser.__parse_dataset(batch_size)
-
-    model = KeyModel(shape, .0004, 100)
-
-
-# Next
-# 1.) Add training
-# 2.) Figure out shape
-
+    pass
